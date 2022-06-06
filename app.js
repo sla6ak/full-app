7829800a047eec10f63e546bf3b2dc39e7c424a7
slabak <slabakxaker@gmail.com>
2022-06-06 17:18:57 +0300

    test server

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(cors());
 app.use("/api-contacts/users", routerAuth);
 app.use("/api-contacts/contacts", routerContacts);
 app.use("/api-contacts/docs/", routerDocs);
+app.get("/api-contacts/", (req, res) => {
+    res.status(250).send(<h1>Welcome - test page!</h1>);
+});
 
 async function start() {
     console.log(process.env.PORT);
